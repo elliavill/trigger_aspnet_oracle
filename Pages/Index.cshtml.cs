181c2a2532b1f70a7037f85db1f935f4d58fe4f7
Aurelia Villyani <aurel.villyani@students.pcci.edu>
2023-03-09 21:57:10 -0600

    not finished System.TypeInitializationException: 'The type initializer for 'OracleInternal.Common.ProviderConfig' threw an exception.'

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,25 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Oracle.ManagedDataAccess.Client;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Data;
 
 namespace TriggerGradeType.Pages
 {
-   public class IndexModel : PageModel
+   public class IndexModel : PageModel 
    {
-      private readonly ILogger<IndexModel> _logger;
-
-      public IndexModel(ILogger<IndexModel> logger)
+      
+      public void OnGet() 
       {
-         _logger = logger;
       }
 
-      public void OnGet()
+      public void OnPostShowInformation()
       {
-
+         using (OracleConnection con = new OracleConnection("User ID=cs306_avillyani;Password=StudyDatabaseWithDrSparks;Data Source=CSORACLE"))
+         {
+               con.Open();
+               OracleCommand cmd = con.CreateCommand();
+               cmd.CommandText = "SELECT * FROM GRADE_TYPE";
+               OracleDataAdapter oda = new OracleDataAdapter(cmd);
+               DataSet dt = new DataSet();
+               oda.Fill(dt);
+               ViewData["myStuff"] = dt.Tables[0];
+         }
       }
-   }
-}
+   } 
+} 
